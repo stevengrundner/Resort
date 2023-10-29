22dd0f7890e95ed459f31ceeb1c973259c7142ed
Steven Grundner <133699580+stevengrundner@users.noreply.github.com>
2023-10-29 07:58:05 -0600

    Delete ski-free directory

--- ski-free/src/main/java/resort/dao/ResortDao.java
+++
@@ -1,9 +0,0 @@
-package resort.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import resort.entity.Resort;
-
-public interface ResortDao extends JpaRepository<Resort, Long> {
-
-}
